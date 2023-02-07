0caab08f61a5ad7ee99620cbbe1519de90aeba78
mohamed khaled <mk6601152@gmail.com>
2023-02-07 14:30:43 +0200

    files updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Container } from "./Components/index";
-import { Header, Footer } from "./Sections/index";
+import { Header, Footer, FeaturedGames } from "./Sections/index";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Home,Browse,Details, Profile,Streams } from "./Pages/index";
--- src/Pages/Browse/Browse.js
+++ src/Pages/Browse/Browse.js
@@ -14,4 +14,6 @@ const Browse = () => {
   )
 }
 
+
+
 export default Browse
